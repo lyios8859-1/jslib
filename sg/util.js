c78b72d62ec26701ebd763cae37bc6d368de69c9
ly <194962218@qq.com>
2018-07-15 00:20:39 +0800

    添加UUID

--- sg/util.js
+++ sg/util.js
@@ -722,3 +722,98 @@ function highURL(menuId, classCur) {
         }
     }
 }
+
+
+/**
+ * var uuid = "cms"+guid();
+ * [S4 生成UUID]
+ */
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+}
+function guid() {
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+/**
+ * 全局唯一标识符（GUID，Globally Unique Identifier）也称作 UUID(Universally Unique IDentifier) 。
+
+GUID是一种由算法生成的二进制长度为128位的数字标识符。
+GUID 的格式为“xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx”，其中的 x 是 0-9 或 a-f 范围内的一个32位十六进制数。
+在理想情况下，任何计算机和计算机集群都不会生成两个相同的GUID。
+
+GUID 的总数达到了2^128（3.4×10^38）个，所以随机生成两个相同GUID的可能性非常小，但并不为0。
+GUID一词有时也专指微软对UUID标准的实现。
+
+ [guid description]
+ * @return {[type]} [description]
+ */
+function guid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
+
+function timeUUID(str){
+    var str = str + "-" || "ly-";
+    var date = new Date();
+    var timestamp = date.getTime();// 时间戳
+    var day = date.getDay();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var milliseconds = date.getMilliseconds();
+    var randomnum = Math.round(Math.random() * 10000);
+    var uuid = str + day + hours + minutes + seconds + milliseconds + timestamp + randomnum ;
+}
+
+function uuid() {
+    var s = [];
+    var hexDigits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    var date = new Date();
+    var timestamp = date.getTime();// 时间戳
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = timestamp;
+ 
+    var uuid = s.join("");
+    return uuid;
+}
+/**
+ * [uuidfreelength 指定长度的uuid]
+ * @param  {[type]} len   [生成的长度]
+ * @param  {[type]} radix [进制数（基数）]
+ * @return {[type]}       [description]
+ */
+function uuidfreelength(len, radix) {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+    var uuid = [], i;
+    radix = radix || chars.length;
+ 
+    if (len) {
+      // Compact form
+      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
+    } else {
+      // rfc4122, version 4 form
+      var r;
+ 
+      // rfc4122 requires these characters
+      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+      uuid[14] = '4';
+ 
+      // Fill in random data.  At i==19 set the high bits of clock sequence as
+      // per rfc4122, sec. 4.1.5
+      for (i = 0; i < 36; i++) {
+        if (!uuid[i]) {
+          r = 0 | Math.random()*16;
+          uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+        }
+      }
+    }
+ 
+    return uuid.join('');
+}
